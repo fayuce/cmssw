aef6789c3fe0ac1e560f77aa957a3c00f8a29460
Adın Soyadın <cern_emailin@cern.ch>
2026-07-14 18:05:10 +0200

    Register HGCal calibration conditions plugin

---
+++ CondCore/HGCalPlugins/plugins/plugin.cc
@@ -0,0 +1,5 @@
+#include "CondCore/ESSources/interface/registration_macros.h"
+#include "CondFormats/DataRecord/interface/HGCalCalibrationRcd.h"
+#include "CondFormats/HGCalObjects/interface/HGCalCalibrationPayload.h"
+
+REGISTER_PLUGIN(HGCalCalibrationRcd, HGCalCalibrationPayload);
